a75fafd69e9178f5d3eb93763d59a721637dbafb
Malina Chichirau <malina.chichirau@gmai.com>
2020-11-24 00:00:16 +0100

    fixed error

--- plotting/plot_overview.py
+++ plotting/plot_overview.py
@@ -60,11 +60,9 @@ def plot_timeline(dates, tweets):
     # ax.scatter(dates, tweets)
     ax.plot(dates, tweets)
 
-    m = str('-0' + str(month) + '-')
     for i in range(len(timeline['date'])): 
-        if m in timeline['date'][i].strftime("%Y-%m-%d"):
-            plt.vlines(x=timeline['date'][i], ymin=0, ymax=ylim, color = 'r')
-            plt.text(timeline['date'][i], ylim/2, timeline['event'][i], rotation=90, verticalalignment='center', fontsize=16)
+        plt.vlines(x=timeline['date'][i], ymin=0, ymax=ylim, color = 'r')
+        plt.text(timeline['date'][i], ylim/2, timeline['event'][i], rotation=90, verticalalignment='center', fontsize=16)
         
     ax.set_xlabel("Dates", fontsize=18)
     ax.set_ylabel("Number of Tweets", fontsize=18)
