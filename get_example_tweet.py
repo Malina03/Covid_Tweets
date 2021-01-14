d93a538f2fe98e361eb5ff52bd6d1543c3ed1b10
Malina Chichirau <malina.chichirau@gmai.com>
2021-01-14 15:46:01 +0100

    force to also look for emojis

--- get_example_tweet.py
+++ get_example_tweet.py
@@ -16,7 +16,6 @@ def write_step_by_step(tweet, nrc, emotag, nlp, f, closed_class, stop_words):
     f.write('\n')
     f.write('Removed as closed class: ')
     for word in nlp(tweet['text']):
-        lemma = word.lemma_
         if word.pos_ in closed_class:
             f.write(word.text + ', ')
     f.write('\n')
@@ -52,25 +51,25 @@ if __name__ == "__main__":
     for _, tweet in data.iterrows():
         if i == 10:
             break
-        if tweet['nrc_joy'] != 0 and tweet['emotag_joy']!= 0:
+        if tweet['nrc_joy'] > 0 and tweet['emotag_joy'] > 0:
             f.write("Double joy example: \n")
             write_step_by_step(tweet, nrc, emotag, nlp, f,closed_class, stop_words)
             i += 1
-        if tweet['nrc_trust'] != 0 and tweet['emotag_trust']!= 0:
+        if tweet['nrc_trust'] > 0 and tweet['emotag_trust'] > 0:
             f.write("Double trust example: \n")
             write_step_by_step(tweet, nrc, emotag, nlp, f,closed_class, stop_words)
             i += 1
-        if tweet['nrc_fear'] != 0:
-            f.write("Fear example: \n")
-            write_step_by_step(tweet, nrc, emotag, nlp, f,closed_class, stop_words)
-            i += 1
-        if tweet['nrc_fear'] != 0 and tweet['emotag_fear']!=0:
+        # if tweet['nrc_fear'] > 0:
+        #     f.write("Fear example: \n")
+        #     write_step_by_step(tweet, nrc, emotag, nlp, f,closed_class, stop_words)
+        #     i += 1
+        if tweet['nrc_fear'] > 0 and tweet['emotag_fear'] > 0:
             f.write("Double ear example: \n")
             write_step_by_step(tweet, nrc, emotag, nlp, f,closed_class, stop_words)
             i += 1
-        if tweet['nrc_anticipation'] != 0:
-            f.write("Anticipation example: \n")
-            write_step_by_step(tweet, nrc, emotag, nlp, f,closed_class, stop_words)
-            i += 1
+        # if tweet['nrc_anticipation'] > 0:
+        #     f.write("Anticipation example: \n")
+        #     write_step_by_step(tweet, nrc, emotag, nlp, f,closed_class, stop_words)
+        #     i += 1
     f.close()
             
