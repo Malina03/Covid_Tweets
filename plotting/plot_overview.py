18796b073201d6f611ef767db6dab778af680af6
Malina Chichirau <malina.chichirau@gmai.com>
2021-01-20 12:11:59 +0100

    fixed format

--- plotting/plot_overview.py
+++ plotting/plot_overview.py
@@ -1,14 +1,13 @@
 import os
 import pandas as pd
-import pickle
+import pickle 
 import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-
 def plot_tweets_per_day(data):
-    months = [2, 3, 4, 5, 6, 7]
+    months = [2,3,4,5,6,7]
     # months = [7]
     dt = []
     tw = []
@@ -16,36 +15,34 @@ def plot_tweets_per_day(data):
         print("plotting month " + str(month))
         dates = []
         tweets = []
-        days = sorted(data[data['month'] == month].day.unique())
+        days = sorted(data[data['month']==month].day.unique())
         # print(days)
-        for day in days:
-            dates.append(datetime.strptime(
-                '2020-' + str(month) + '-' + str(day), "%Y-%m-%d").date())
-            tweets.append(
-                len(data[(data['month'] == month) & (data['day'] == day)]['text']))
+        for day in days: 
+            dates.append(datetime.strptime('2020-' + str(month) + '-' + str(day), "%Y-%m-%d").date())
+            tweets.append(len(data[(data['month']==month) & (data['day']==day)]['text']))
         # plot_monthly_timeline(dates, tweets, month)
         dt = dt + dates
         tw = tw + tweets
     plot_timeline(dt, tw)
 
-
 def plot_timeline(dates, tweets):
     if len(tweets) == 0:
         return
-    months = {2: 'february', 3: 'march',
-        4: 'april', 5: 'may', 6: 'june', 7: 'july'}
+    months = {2:'february', 3:'march', 4:'april', 5:'may', 6:'june', 7:'july'}
    important_dates = np.array([datetime.strptime('2020-02-20', "%Y-%m-%d").date(), 
-                               datetime.strptime('2020-02-23', "%Y-%m-%d").date(), 
-                               datetime.strptime('2020-03-04', "%Y-%m-%d").date(),
-                               datetime.strptime('2020-03-09', "%Y-%m-%d").date(),                              
-                               datetime.strptime('2020-03-22', "%Y-%m-%d").date(),
-                               datetime.strptime('2020-03-31', "%Y-%m-%d").date()
-                               datetime.strptime('2020-04-05', "%Y-%m-%d").date(),
-                               datetime.strptime('2020-04-20', "%Y-%m-%d").date()
-                               datetime.strptime('2020-05-04', "%Y-%m-%d").date(),
-                               datetime.strptime('2020-06-15', "%Y-%m-%d").date(), 
-                               datetime.strptime('2020-07-02', "%Y-%m-%d").date(),
-                               datetime.strptime('2020-07-14', "%Y-%m-%d").date()])
+                                datetime.strptime('2020-02-23', "%Y-%m-%d").date(), 
+                                datetime.strptime('2020-03-04', "%Y-%m-%d").date(),
+                                # datetime.strptime('2020-03-08', "%Y-%m-%d").date(),
+                                datetime.strptime('2020-03-09', "%Y-%m-%d").date(),
+                                # datetime.strptime('2020-03-11', "%Y-%m-%d").date(),
+                                datetime.strptime('2020-03-22', "%Y-%m-%d").date(),
+                                datetime.strptime('2020-03-31', "%Y-%m-%d").date()
+                                datetime.strptime('2020-04-05', "%Y-%m-%d").date(),
+                                datetime.strptime('2020-04-20', "%Y-%m-%d").date()
+                                datetime.strptime('2020-05-04', "%Y-%m-%d").date(),
+                                datetime.strptime('2020-06-15', "%Y-%m-%d").date(), 
+                                datetime.strptime('2020-07-02', "%Y-%m-%d").date(),
+                                datetime.strptime('2020-07-14', "%Y-%m-%d").date()])
     events = np.array(['20th Feb: Third confirmed case', '23rd Feb: Venice Carnival is cancelled', '4th March: Schools and Universities close', 
                         '9th March: Nationwide Lockdown', '22nd March: Nonessential Factories close', '31st March: Peak of the Pandemic announced',
                         '5th April: Decrease of Daily Deaths', '20th April: Decrease of Active Cases','4th May: Restrictions are relaxed',
